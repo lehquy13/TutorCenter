d66904f77f7c7c2cab9366ddcae15d95976b3a54
Lê Hoàng Quý <91002362+lehquy13@users.noreply.github.com>
2023-09-19 21:31:48 +0700

    Add clientProject and setup dependencies

---
+++ TutorCenter.Client/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TutorCenter.Client.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
