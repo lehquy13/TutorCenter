4a3e3204b0b84fff83a8e709bd22978a99c6fba6
lehquy13 <lehquy13@gmail.com>
2023-12-20 16:17:07 +0700

    Update UI

---
+++ TutorCenter.Infrastructure/Migrations/20231207033639_Update tutor table.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TutorCenter.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Updatetutortable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TutorId",
+                table: "Course",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Course_TutorId",
+                table: "Course",
+                column: "TutorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Course_Tutor_TutorId",
+                table: "Course",
+                column: "TutorId",
+                principalTable: "Tutor",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Course_Tutor_TutorId",
+                table: "Course");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Course_TutorId",
+                table: "Course");
+
+            migrationBuilder.DropColumn(
+                name: "TutorId",
+                table: "Course");
+        }
+    }
+}
